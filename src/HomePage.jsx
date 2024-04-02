f7f8c97836fcd948a4ebb0760617fbe59bed4a54
lnirjar <lnirjar@gmail.com>
2024-04-03 02:03:06 +0530

    added helmet

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,21 +1,27 @@
 import { Link } from "react-router-dom"
 import { challengesData } from "./main"
+import { Helmet } from "react-helmet"
 
 export const HomePage = () => {
   return (
-    <div className="p-6 bg-neutral-200 min-h-screen">
-      <h1 className="text-4xl font-bold">Frontend Mentor Challenges</h1>
-      <ul className="text-2xl mt-4">
-        {
-          challengesData.map(challenge => (
-            <li key={challenge.path} className="hover:underline">
-              <Link to={challenge.path}>
-                {challenge.name}
-              </Link>
-            </li>
-          ))
-        }
-      </ul>
-    </div>
+    <>
+      <Helmet>
+        <title>Frontend Mentor | React</title>
+      </Helmet>
+      <div className="p-6 bg-neutral-200 min-h-screen">
+        <h1 className="text-4xl font-bold">Frontend Mentor Challenges</h1>
+        <ul className="text-2xl mt-4">
+          {
+            challengesData.map(challenge => (
+              <li key={challenge.path} className="hover:underline">
+                <Link to={challenge.path}>
+                  {challenge.name}
+                </Link>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+    </>
   )
 }
--- src/challenges/blog-preview-card/BlogPreviewCardPage.jsx
+++ src/challenges/blog-preview-card/BlogPreviewCardPage.jsx
@@ -1,9 +1,15 @@
+import { Helmet } from "react-helmet"
 import { Card } from "./components/Card"
 
 export const BlogPreviewCardPage = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen p-6 bg-[#F4D04E] font-figtree">
-      <Card />
-    </div>
+    <>
+      <Helmet>
+        <title>Frontend Mentor | Blog preview card</title>
+      </Helmet>
+      <div className="flex justify-center items-center min-h-screen p-6 bg-[#F4D04E] font-figtree">
+        <Card />
+      </div>
+    </>
   )
 }
